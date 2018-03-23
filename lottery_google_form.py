0f1b6aee02005d060b3801be9d100ea83a21b650
luckyaddress <deathkao@gmail.com>
2018-03-23 16:01:11 +0800

    commit

--- lottery_google_form.py
+++ lottery_google_form.py
@@ -1,4 +1,12 @@
 #-*- coding: utf-8 -*- 
+# 使用方式 開啟從Google表單擷取下來的結果 執行此py檔案，給予兩個參數 第一個要抽出幾份獎品的數字
+# 第二個要將結果存為XXXXX.csv
+# 指令範例 cat lottery_list.csv | python lottery_google_form.py 3  246.csv
+# Google試算表存檔範例為   
+# 職號  分數
+# 123   100
+# 124    95
+
 import csv,sys,random # 匯入csv 跟 亂數模組random
 
 get_list = sys.stdin.readlines() # 接收標準輸入 取得抽獎名單
@@ -20,7 +28,17 @@ for data in gln_n:
     if times == 1 and data.split(",")[1] == "100":
         random_list.append(data.split(",")[0])
 
-print(random_list)   # 列出得獎人質號
+print(random_list)   # 列出得獎人職號
 result_list = random.sample(random_list, int(sys.argv[1])) # 後面命令列參數為要抽幾個得獎者 
 # random.sample的用法 random.sample(樣本群，要抽的個數--接受命令列參數)
-print(result_list)   # 抽出得獎人質號
+print(result_list)   # 抽出得獎人職號
+
+
+result_save = open(sys.argv[2],"w") # 將抽獎結果存在 命令列參數指定的檔案中
+
+title = "得獎職號\r\n"   # 組出 抽獎結果的csv
+result_save.write(title)  
+
+for emp_no in result_list:
+    result_save.write(emp_no+"\r\n")   
+
---
+++ lottery_wm.py
@@ -0,0 +1,46 @@
+#-*- coding: utf-8 -*- 
+import csv,sys,random, codecs # 匯入csv 跟 亂數模組random
+
+wm_result = sys.stdin.readlines() # 接收標準輸入 取得第一階段名單
+
+del wm_result[0]
+
+wm_final = []
+
+for num in wm_result:
+    no = str(num).strip("\r\n")   # 將兩階段的名單混合在一起  並接著加入陣列
+    wm_final.append(no)
+    
+wm_final_n = set(wm_final)        # 第二階段中如果有分數跟職號完全相同的，用set將之去除
+
+wm_list = []
+
+for wm_data in wm_final_n:
+    times = str(wm_final_n).count(wm_data.split(",")[0])   # 每個人在第二階段的職號也能出現一次
+    if times == 1 and len(wm_data.split(",")) == 2 :       # 只去取第二階段有來考試的人並且第一階段沒中獎的人
+        wm_list.append(wm_data.split(","))                 # (跟上方職號一次取交集) 即可去除掉第一階段中獎還來考試的
+    
+
+wm_list_100 = []            # 接著處理過濾掉 沒有考100滿分的人
+
+for x in wm_list:
+    if x[1] == "100":
+        wm_list_100.append(x[0])  # 取得第一階段沒抽中 第二階段有來考 並只紀錄一次 考100的職號
+
+print(wm_list_100)
+
+getprize_list = random.sample(wm_list_100, int(sys.argv[1])) # 後面命令列參數為要抽幾個得獎者 
+# random.sample的用法 random.sample(樣本群，要抽的個數--接受命令列參數)
+
+print(getprize_list)  #列出得獎人職號
+
+
+result_save = open(sys.argv[2],"w") # 將抽獎結果存在 命令列參數指定的檔案中
+
+title = "得獎職號\r\n"   # 組出 抽獎結果的csv
+result_save.write(title)  
+
+for emp_no in getprize_list:
+    result_save.write(emp_no+"\r\n")   
+
+        
